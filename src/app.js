131223e9d326a74c42e75e5af61f3613871b6f79
omar saadane el meski <o_omar_96@hotmail.com>
2023-11-17 20:37:02 +0000

    ejercicio pendiente cards

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let iconTop = document.getElementById("palos");
+  let iconBotton = document.getElementById("palos");
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let randomPalos = [Math.floor(Math.random() * palos.length)];
+  let randomNumeros = [Math.floor(Math.random() * numeros.length)];
+
+  let elementPalos = document.getElementsByClassName("palos");
+  let elementnumeros = document.getElementsByClassName("numeros");
+
+  elementPalos[0].innerHTML = palos[randomPalos];
+  elementPalos[1].innerHTML = palos[randomPalos];
+
+  document.querySelector(".numeros").innerHTML = randomNumeros;
 };
